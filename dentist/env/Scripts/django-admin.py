70b127f06c83f2cf57ba7ed1613e2f7b416d6bfe
alegoriyass <alegoriyass@yandex.ru>
2020-08-04 14:27:17 +0300

    Add project files.

---
+++ dentist/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\СтеблинаЕЕ\source\repos\Django\dentist\dentist\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
