cf6f5af3263aea94fd2591069d009282f6eff004
alegoriyass <alegoriyass@yandex.ru>
2020-10-02 09:29:07 +0300

    model

--- dentist/website/urls.py
+++ dentist/website/urls.py
@@ -7,5 +7,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('contact.html', views.contact, name="contact")
+    path('contact.html', views.contact, name="contact"),
+    path('about.html', views.about, name="about"),
+    path('pricing.html', views.pricing, name="pricing"),
+    path('service.html', views.service, name="service"),
+    path('appointment.html', views.appointment, name="appointment"),
 ]
--- dentist/website/views.py
+++ dentist/website/views.py
@@ -23,3 +23,45 @@ def contact(request):
 
     else:
         return render(request, 'website/contact.html', {})
+
+def about(request):
+    return render(request, 'website/about.html', {})
+
+def pricing(request):
+    return render(request, 'website/pricing.html', {})
+
+def service(request):
+    return render(request, 'website/service.html', {})
+
+def appointment(request):
+    if request.method == "POST":
+        your_name = request.POST['your-name']
+        your_phone = request.POST['your-phone']
+        your_email = request.POST['your-email']
+        your_address = request.POST['your-address']
+        your_schedule = request.POST['your-schedule']
+        your_date = request.POST['your-date']
+        your_message = request.POST['your-message']
+
+        appointment = 'Name: ' + your_name + '\n Phone: ' + your_phone + '\n Email:' + your_email + '\n Address: ' + your_address + '\n When: ' + your_schedule + '\n Day: ' + your_date + '\n Message: ' + your_message
+        
+        
+        # send an email
+        send_mail(     
+            'Appointment Request', # subject
+            appointment, # message
+            your_email, # from email
+            ['egorinkas@gmail.com'], # To Email
+            )
+
+        return render(request, 'website/appointment.html', {     
+            'your_name': your_name,
+            'your_phone': your_phone,
+            'your_email': your_email,
+            'your_address': your_address,
+            'your_schedule': your_schedule,
+            'your_date': your_date,
+            'your_message': your_message,
+            })
+    else:
+        return render(request, 'website/home.html', {})
